6aae7fb34f2b80cc98836b7df9f27f35f0a64c7f
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-06-12 08:15:21 +0300

    Refactor

--- configuration.go
+++ configuration.go
@@ -5,62 +5,77 @@ import (
 )
 
 type (
-	Server struct {
+	server struct {
 		Port string `json:"port"`
 		User string `json:"user"`
-		Ip   string `json:"ip"`
+		IP   string `json:"ip"`
 	}
-	Environment struct {
+	environment struct {
 		Name               string   `json:"name"`
-		Servers            []Server `json:"servers"`
+		Servers            []server `json:"servers"`
 		RestartCommand     string   `json:"restart_command"`
 		DeployDir          string   `json:"deploydir"`
 		AfterDeployCommand string   `json:"after_deploy_command"`
 	}
-	Campfire struct {
+	campfire struct {
 		Account string `json:"account"`
 		Token   string `json:"token"`
 		Rooms   string `json:"rooms"`
 	}
-	Configuration struct {
+	configuration struct {
 		CrapVersion        string        `json:"crap_version"`
-		Environments       []Environment `json:"environments"`
+		Environments       []environment `json:"environments"`
 		BuiltAppDir        string        `json:"built_app_dir"`
 		AppBuildCommands   []string      `json:"app_build_commands"`
 		AssetBuildCommands []string      `json:"asset_build_commands"`
-		Campfire           Campfire
+		Campfire           campfire
 	}
 )
 
-func NewSampleConfiguration() *Configuration {
-	return &Configuration{
-		CrapVersion: Version,
-		Environments: []Environment{
-			Environment{
+func newSampleConfiguration() *configuration {
+	return &configuration{
+		CrapVersion: version,
+		Environments: []environment{
+			environment{
 				Name: "staging",
-				Servers: []Server{
-					Server{Port: "22", User: "deployment", Ip: "127.0.0.1"},
-					Server{Port: "22", User: "deployment", Ip: "localhost"}},
+				Servers: []server{
+					server{
+						Port: "22",
+						User: "deployment",
+						IP:   "localhost",
+					},
+				},
 				RestartCommand: "(sudo stop myapp_staging || true) && sudo start myapp_staging",
 				DeployDir:      "/var/www/myapp"},
-			Environment{
+			environment{
 				Name: "production",
-				Servers: []Server{
-					Server{Port: "22", User: "deployment", Ip: "www.myapp.com"}},
+				Servers: []server{
+					server{
+						Port: "22",
+						User: "deployment",
+						IP:   "www.myapp.com",
+					},
+				},
 				DeployDir:          "/var/www/myapp",
 				RestartCommand:     "(sudo stop myapp_production || true) && sudo start myapp_production",
-				AfterDeployCommand: "make cleanup"}},
+				AfterDeployCommand: "make cleanup",
+			},
+		},
 		AppBuildCommands:   []string{"make linux64bit"},
 		AssetBuildCommands: []string{"make css_assets_gzip", "make js_assets_gzip"},
 		BuiltAppDir:        "out/myapp",
-		Campfire:           Campfire{Account: "mycampfireaccount", Token: "foobarfoobarfoobar", Rooms: "8343,234223"},
+		Campfire: campfire{
+			Account: "mycampfireaccount",
+			Token:   "foobarfoobarfoobar",
+			Rooms:   "8343,234223",
+		},
 	}
 }
 
-func (server *Server) Host() string {
-	return fmt.Sprintf("%s@%s", server.User, server.Ip)
+func (s *server) Host() string {
+	return fmt.Sprintf("%s@%s", s.User, s.IP)
 }
 
-func (server *Server) Socket() string {
-	return fmt.Sprintf("%s:%s", server.Host(), server.Port)
+func (s *server) Socket() string {
+	return fmt.Sprintf("%s:%s", s.Host(), s.Port)
 }
--- crap.go
+++ crap.go
@@ -19,14 +19,14 @@ import (
 )
 
 const (
-	ConfigurationFile = "crap.json"
-	Version           = "0.9"
+	configurationFile = "crap.json"
+	version           = "0.8"
 )
 
 var (
-	crapify = flag.Bool("crapify", false, "create a new configuration file")
-	version = flag.Bool("version", false, "print version and exit")
-	verbose = flag.Bool("verbose", false, "verbose logging")
+	crapify      = flag.Bool("crapify", false, "create a new configuration file")
+	printVersion = flag.Bool("version", false, "print version and exit")
+	verbose      = flag.Bool("verbose", false, "verbose logging")
 )
 
 func main() {
@@ -34,227 +34,67 @@ func main() {
 
 	flag.Parse()
 
-	if *version {
-		fmt.Println(Version)
-		os.Exit(0)
-	}
-
-	// Create a new config file if required
-	if *crapify {
-		conf := NewSampleConfiguration()
-		b, err := json.MarshalIndent(conf, "", "  ")
-		if err != nil {
-			panic(err)
-		}
-		if err := ioutil.WriteFile(ConfigurationFile, b, 0644); err != nil {
-			panic(err)
-		}
-		os.Exit(0)
-	}
-
-	// Parse config
-	b, err := ioutil.ReadFile(ConfigurationFile)
-	if err != nil {
-		fmt.Println("Could not open configuration file (crap.json):", err)
-		fmt.Println("Hint: pass --crapify to create a new configuration file")
-		os.Exit(1)
-	}
-	var conf Configuration
-	if err := json.Unmarshal(b, &conf); err != nil {
-		panic(err)
-	}
-
-	// Select environment
-	args := flag.Args()
-	if len(args) == 0 {
-		fmt.Println("Specify an environment")
-		os.Exit(1)
-	}
-	var env *Environment
-	for _, e := range conf.Environments {
-		if e.Name == args[0] {
-			env = &e
-			break
-		}
-	}
-	if env == nil {
-		fmt.Println("Environment", args[0], "not found")
-		os.Exit(1)
-	}
-
-	// Validate config
-	if len(env.Servers) != 1 {
-		fmt.Println("No server(s) in environment configuration. Please specify one!")
-		os.Exit(1)
-	}
-	if len(conf.AssetBuildCommands) == 0 && len(conf.AppBuildCommands) == 0 {
-		fmt.Println("No asset_build_commands or app_build_commands found in environment configuration")
-		os.Exit(1)
-	}
-	if len(env.DeployDir) == 0 {
-		fmt.Println("deploydir must be filled in!")
-		os.Exit(1)
-	}
-	if len(conf.CrapVersion) == 0 {
-		fmt.Println("Your crap.json file is unversioned - please add crap_version to your crap.json")
-		os.Exit(1)
-	}
-	if conf.CrapVersion != Version {
-		fmt.Println("Your crap.json requires crap", conf.CrapVersion, "but this crap is", Version, "- please upgrade!")
-		os.Exit(1)
-	}
-
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	if *verbose {
 		log.Println("GOMAXPROCS", runtime.GOMAXPROCS(0))
 	}
 
+	if *printVersion {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
+	if *crapify {
+		createSampleConfig()
+		os.Exit(0)
+	}
+
+	conf := parseConfig()
+	conf.validate()
+
+	env := conf.selectEnvironment()
+	env.validate()
+
 	cmd := "if [ ! -d out ]; then mkdir -p out; fi"
-	run("Create out dir", exec.Command("/bin/sh", "-c", cmd))
+	run("create out dir", exec.Command("/bin/sh", "-c", cmd))
 
-	server := env.Servers[0]
+	server := &(env.Servers[0])
 
-	// Kick of SSH ControlMaster in the background
-	controlMasterStarted := make(chan bool)
-	go func() {
-		cmd := fmt.Sprintf("ssh -nNf -o \"ControlMaster=yes\" -o \"ControlPath=%s\" -p %s %s", server.Socket(), server.Port, server.Host())
-		run("Start SSH ControlMaster", exec.Command("sh", "-c", cmd))
-		controlMasterStarted <- true
-	}()
-
+	controlMasterStarted := server.startSSHControlMaster()
 	defer func() {
-		controlMasterStopped := make(chan bool)
-		go func() {
-			cmd := fmt.Sprintf("ssh -O exit -S '%s' -p %s %s", server.Socket(), server.Port, server.Host())
-			run("Stop SSH ControlMaster", exec.Command("sh", "-c", cmd))
-			controlMasterStopped <- true
-		}()
-
-		<-controlMasterStopped
+		server.stopSSHControlMaster()
 	}()
 
-	// Start building in the background
-	executeBuildCommand := func(buildCommand string, ready chan bool) {
-		run("Build command: "+buildCommand, exec.Command("sh", "-c", buildCommand))
-		ready <- true
-	}
-	buildAll := func(buildCommands []string) chan bool {
-		ready := make(chan bool, len(buildCommands))
-		for _, buildCommand := range buildCommands {
-			go executeBuildCommand(buildCommand, ready)
-		}
-		return ready
-	}
-	appBuildReady := buildAll(conf.AppBuildCommands)
-	assetBuildReady := buildAll(conf.AssetBuildCommands)
+	appBuildReady := build(conf.AppBuildCommands)
+	assetBuildReady := build(conf.AssetBuildCommands)
 
-	// Construct release dir
 	releaseBasePath := filepath.Join(env.DeployDir, "releases")
 	releaseDir := filepath.Join(releaseBasePath, time.Now().Format("20060102150405"))
 
-	// Prepare servers
-	serverPrepared := make(chan bool)
-	go func() {
-		// Run a bunch of commands on the remote server. Set up shared dir, symlinks etc
-		var buffer bytes.Buffer
-
-		cmd := fmt.Sprintf("if [ ! -d %s/shared/log ]; then mkdir -p %s/shared/log; fi", env.DeployDir, env.DeployDir)
-		buffer.WriteString(cmd)
-
-		cmd = fmt.Sprintf(" && if [ ! -d %s ]; then mkdir -p %s; fi", releaseDir, releaseDir)
-		buffer.WriteString(cmd)
-
-		cmd = fmt.Sprintf(" && chmod -R g+w %s", releaseDir)
-		buffer.WriteString(cmd)
-
-		cmd = fmt.Sprintf(" && (rm -rf %s/public/system || true) && mkdir -p %s/public/", releaseDir, releaseDir)
-		buffer.WriteString(cmd)
-
-		cmd = fmt.Sprintf(" && ln -s %s/shared/system %s/public/system", env.DeployDir, releaseDir)
-		buffer.WriteString(cmd)
-
-		cmd = fmt.Sprintf(" && rm -rf %s/log", releaseDir)
-		buffer.WriteString(cmd)
-
-		cmd = fmt.Sprintf(" && ln -s %s/shared/log %s/log", env.DeployDir, releaseDir)
-		buffer.WriteString(cmd)
-
-		cmd = fmt.Sprintf(" && rm -rf %s/tmp/pids && mkdir -p %s/tmp/", releaseDir, releaseDir)
-		buffer.WriteString(cmd)
-
-		cmd = fmt.Sprintf(" && ln -s %s/shared/pids %s/tmp/pids", env.DeployDir, releaseDir)
-		buffer.WriteString(cmd)
-
-		if len(conf.AssetBuildCommands) > 0 {
-			cmd = fmt.Sprintf(" && rm -rf %s/public/assets && mkdir -p %s/public && mkdir -p %s/shared/assets && ln -s %s/shared/assets %s/public/assets",
-				releaseDir, releaseDir, env.DeployDir, env.DeployDir, releaseDir)
-			buffer.WriteString(cmd)
-		}
-
-		run("Prepare server", exec.Command("ssh", "-p", server.Port, "-S", server.Socket(), "-l", server.User, server.Ip, buffer.String()))
-		serverPrepared <- true
-	}()
+	serverPrepared := server.prepareServer(env, releaseDir, len(conf.AssetBuildCommands) > 0)
 
 	<-controlMasterStarted
 	<-serverPrepared
 
-	// Rsync assets in the background
 	assetsRsynced := make(chan bool)
 	go func() {
-		for _ = range conf.AssetBuildCommands {
-			<-assetBuildReady
-		}
-		if len(conf.AssetBuildCommands) > 0 {
-			cmd := fmt.Sprintf("rsync -e 'ssh -p %s -S \"%s\"' --recursive --times --compress --human-readable public/assets %s:%s/shared",
-				server.Port, server.Socket(), server.Host(), env.DeployDir)
-			run("Rsync assets", exec.Command("/bin/sh", "-c", cmd))
-		}
+		<-assetBuildReady
+		<-server.rsyncAssets(env, conf)
 		assetsRsynced <- true
 	}()
 
-	// Build app
-	appBuilt := make(chan bool)
-	go func() {
-		for _ = range conf.AppBuildCommands {
-			<-appBuildReady
-		}
-		appBuilt <- true
-	}()
-
-	// Rsync app files to sever
 	appUploaded := make(chan bool)
 	go func() {
-		<-appBuilt
-		cmd := fmt.Sprintf("rsync -e 'ssh -p %s -S \"%s\"' --recursive --times --compress --human-readable dist %s:%s/shared",
-			server.Port, server.Socket(), server.Host(), env.DeployDir)
-		run("Rsync app files", exec.Command("/bin/sh", "-c", cmd))
+		<-appBuildReady
+		<-server.uploadApp(env)
 		appUploaded <- true
 	}()
 
-	<-appUploaded
-	<-assetsRsynced
-
-	// Finalize deploy
 	appFinalized := make(chan bool)
 	go func() {
-		var finalize bytes.Buffer
-
-		// Copy app files into release dir
-		cmd = fmt.Sprintf("cp -r %s %s", filepath.Join(env.DeployDir, "shared", "dist", "*"), releaseDir)
-		finalize.WriteString(cmd)
-
-		// Replace symlink
-		symlink := filepath.Join(env.DeployDir, "current")
-		finalize.WriteString(fmt.Sprintf(" && rm -f %s", symlink))
-		finalize.WriteString(fmt.Sprintf(" && ln -s %s %s", releaseDir, symlink))
-
-		if len(env.RestartCommand) > 0 {
-			cmd := fmt.Sprintf(" && (%s)", env.RestartCommand)
-			finalize.WriteString(cmd)
-		}
-
-		run("Finalize server", exec.Command("ssh", "-p", server.Port, "-S", fmt.Sprintf("'%s'", server.Socket()), "-l", server.User, server.Ip, finalize.String()))
-
+		<-appUploaded
+		<-assetsRsynced
+		<-server.finalizeApp(env, releaseDir)
 		appFinalized <- true
 	}()
 
@@ -265,7 +105,7 @@ func main() {
 	}
 
 	deployDuration := time.Since(deployStart)
-	log.Println("App deployed to", env.Name, "in", deployDuration)
+	log.Println("app deployed to", env.Name, "("+deployDuration.String()+")")
 
 	if conf.Campfire.Account != "" && conf.Campfire.Token != "" && conf.Campfire.Rooms != "" {
 		if currentUser, err := user.Current(); err != nil {
@@ -303,4 +143,204 @@ func run(label string, cmd *exec.Cmd) {
 	}
 	since := time.Since(cmdStart)
 	log.Println(label, "("+since.String()+")")
+}
+
+func (s *server) startSSHControlMaster() chan bool {
+	done := make(chan bool)
+	go func() {
+		cmd := fmt.Sprintf("ssh -nNf -o \"ControlMaster=yes\" -o \"ControlPath=%s\" -p %s %s", s.Socket(), s.Port, s.Host())
+		run("start SSH ControlMaster", exec.Command("sh", "-c", cmd))
+		done <- true
+	}()
+	return done
+}
+
+func (s *server) finalizeApp(env *environment, releaseDir string) chan bool {
+	done := make(chan bool)
+	go func() {
+		var finalize bytes.Buffer
+
+		// Copy app files into release dir
+		cmd := fmt.Sprintf("cp -r %s %s", filepath.Join(env.DeployDir, "shared", "dist", "*"), releaseDir)
+		finalize.WriteString(cmd)
+
+		// Replace symlink
+		symlink := filepath.Join(env.DeployDir, "current")
+		finalize.WriteString(fmt.Sprintf(" && rm -f %s", symlink))
+		finalize.WriteString(fmt.Sprintf(" && ln -s %s %s", releaseDir, symlink))
+
+		if len(env.RestartCommand) > 0 {
+			cmd := fmt.Sprintf(" && (%s)", env.RestartCommand)
+			finalize.WriteString(cmd)
+		}
+
+		run("finalize server", exec.Command("ssh", "-p", s.Port, "-S", fmt.Sprintf("'%s'", s.Socket()), "-l", s.User, s.IP, finalize.String()))
+
+		done <- true
+	}()
+	return done
+}
+
+func (s *server) uploadApp(env *environment) chan bool {
+	done := make(chan bool)
+	go func() {
+		cmd := fmt.Sprintf("rsync -e 'ssh -p %s -S \"%s\"' --recursive --times --compress --human-readable dist %s:%s/shared",
+			s.Port, s.Socket(), s.Host(), env.DeployDir)
+		run("rsync app files", exec.Command("/bin/sh", "-c", cmd))
+		done <- true
+	}()
+	return done
+}
+
+func (s *server) prepareServer(env *environment, releaseDir string, hasBuildCommands bool) chan bool {
+	done := make(chan bool)
+	go func() {
+		// Run a bunch of commands on the remote server. Set up shared dir, symlinks etc
+		var buffer bytes.Buffer
+
+		cmd := fmt.Sprintf("if [ ! -d %s/shared/log ]; then mkdir -p %s/shared/log; fi", env.DeployDir, env.DeployDir)
+		buffer.WriteString(cmd)
+
+		cmd = fmt.Sprintf(" && if [ ! -d %s ]; then mkdir -p %s; fi", releaseDir, releaseDir)
+		buffer.WriteString(cmd)
+
+		cmd = fmt.Sprintf(" && chmod -R g+w %s", releaseDir)
+		buffer.WriteString(cmd)
+
+		cmd = fmt.Sprintf(" && (rm -rf %s/public/system || true) && mkdir -p %s/public/", releaseDir, releaseDir)
+		buffer.WriteString(cmd)
+
+		cmd = fmt.Sprintf(" && ln -s %s/shared/system %s/public/system", env.DeployDir, releaseDir)
+		buffer.WriteString(cmd)
+
+		cmd = fmt.Sprintf(" && rm -rf %s/log", releaseDir)
+		buffer.WriteString(cmd)
+
+		cmd = fmt.Sprintf(" && ln -s %s/shared/log %s/log", env.DeployDir, releaseDir)
+		buffer.WriteString(cmd)
+
+		cmd = fmt.Sprintf(" && rm -rf %s/tmp/pids && mkdir -p %s/tmp/", releaseDir, releaseDir)
+		buffer.WriteString(cmd)
+
+		cmd = fmt.Sprintf(" && ln -s %s/shared/pids %s/tmp/pids", env.DeployDir, releaseDir)
+		buffer.WriteString(cmd)
+
+		if hasBuildCommands {
+			cmd = fmt.Sprintf(" && rm -rf %s/public/assets && mkdir -p %s/public && mkdir -p %s/shared/assets && ln -s %s/shared/assets %s/public/assets",
+				releaseDir, releaseDir, env.DeployDir, env.DeployDir, releaseDir)
+			buffer.WriteString(cmd)
+		}
+
+		run("prepare server", exec.Command("ssh", "-p", s.Port, "-S", s.Socket(), "-l", s.User, s.IP, buffer.String()))
+		done <- true
+	}()
+	return done
+}
+
+func createSampleConfig() {
+	conf := newSampleConfiguration()
+	b, err := json.MarshalIndent(conf, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	if err := ioutil.WriteFile(configurationFile, b, 0644); err != nil {
+		panic(err)
+	}
+}
+
+func parseConfig() *configuration {
+	b, err := ioutil.ReadFile(configurationFile)
+	if err != nil {
+		fmt.Println("Could not open configuration file (crap.json):", err)
+		fmt.Println("Hint: pass --crapify to create a new configuration file")
+		os.Exit(1)
+	}
+	var conf configuration
+	if err := json.Unmarshal(b, &conf); err != nil {
+		panic(err)
+	}
+	return &conf
+}
+
+func (conf *configuration) selectEnvironment() *environment {
+	args := flag.Args()
+	if len(args) == 0 {
+		fmt.Println("Specify an environment")
+		os.Exit(1)
+	}
+	var env *environment
+	for _, e := range conf.Environments {
+		if e.Name == args[0] {
+			env = &e
+			break
+		}
+	}
+	if env == nil {
+		fmt.Println("Environment", args[0], "not found")
+		os.Exit(1)
+	}
+	return env
+}
+
+func (env *environment) validate() {
+	if len(env.DeployDir) == 0 {
+		fmt.Println("deploydir must be filled in!")
+		os.Exit(1)
+	}
+	if len(env.Servers) != 1 {
+		fmt.Println("No server(s) in environment configuration. Please specify one!")
+		os.Exit(1)
+	}
+}
+
+func (conf *configuration) validate() {
+	if len(conf.AssetBuildCommands) == 0 && len(conf.AppBuildCommands) == 0 {
+		fmt.Println("No asset_build_commands or app_build_commands found in environment configuration")
+		os.Exit(1)
+	}
+	if len(conf.CrapVersion) == 0 {
+		fmt.Println("Your crap.json file is unversioned - please add crap_version to your crap.json")
+		os.Exit(1)
+	}
+	if conf.CrapVersion != version {
+		fmt.Println("Your crap.json requires crap", conf.CrapVersion, "but this crap is", version, "- please upgrade!")
+		os.Exit(1)
+	}
+}
+
+func (s *server) stopSSHControlMaster() {
+	cmd := fmt.Sprintf("ssh -O exit -S '%s' -p %s %s", s.Socket(), s.Port, s.Host())
+	run("stop SSH ControlMaster", exec.Command("sh", "-c", cmd))
+}
+
+func (s *server) rsyncAssets(env *environment, conf *configuration) chan bool {
+	done := make(chan bool)
+	go func() {
+		if len(conf.AssetBuildCommands) > 0 {
+			cmd := fmt.Sprintf("rsync -e 'ssh -p %s -S \"%s\"' --recursive --times --compress --human-readable public/assets %s:%s/shared",
+				s.Port, s.Socket(), s.Host(), env.DeployDir)
+			run("rsync assets", exec.Command("/bin/sh", "-c", cmd))
+		}
+		done <- true
+	}()
+	return done
+}
+
+func build(buildCommands []string) chan bool {
+	done := make(chan bool)
+	buildOne := func(buildCommand string, done chan bool) {
+		run(buildCommand, exec.Command("sh", "-c", buildCommand))
+		done <- true
+	}
+	go func() {
+		builds := make(chan bool, len(buildCommands))
+		for _, cmd := range buildCommands {
+			go buildOne(cmd, builds)
+		}
+		for _ = range buildCommands {
+			<-builds
+		}
+		done <- true
+	}()
+	return done
 }
